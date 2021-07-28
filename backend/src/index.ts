b2d6741d1410b10b8ffce8dc5a8d2ff5908604ad
nirewen <eduardolonderovieira@gmail.com>
2021-07-27 23:22:16 -0300

    Serve in the main file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import cors from 'cors'
 import session from 'express-session'
 import passport from 'passport'
@@ -47,12 +48,19 @@ app.use(
         name: 'discord-token',
     })
 )
+
 app.use(passport.initialize())
 app.use(passport.session())
 
-if (process.env.NODE_ENV === 'production') app.use(frontend)
-
 app.use('/api', routes)
+
+if (process.env.ENVIRONMENT === 'production') {
+    app.use(express.static(path.join(__dirname, 'public')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    })
+}
 
 const server = app.listen(port, () => console.log('Listening on port ' + port))
 
