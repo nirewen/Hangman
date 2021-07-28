9ddac4964782123b4fd68614e0c129cbfe04b517
nirewen <eduardolonderovieira@gmail.com>
2021-07-28 18:55:46 -0300

    Fix socket URL

--- frontend/src/providers/Socket.tsx
+++ frontend/src/providers/Socket.tsx
@@ -1,12 +1,12 @@
 import React, { createContext, useContext } from 'react'
 import { io, Socket } from 'socket.io-client'
 
-const { API_URL } = process.env
+const { REACT_APP_API_URL } = process.env
 
 const SocketContext = createContext<Socket | null>(null)
 
 const SocketProvider: React.FC = ({ children }) => {
-    const socket = io(API_URL!)
+    const socket = io(REACT_APP_API_URL!)
 
     return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
 }
